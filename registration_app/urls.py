04c552aa862ba4f49bb21b06adbc30915a2170c9
Julian Jose <julianjose.202@gmail.com>
2023-11-14 02:29:08 +0530

    update direct to registration page

--- registration_app/urls.py
+++ registration_app/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import registration_form, confirmation_page
 
 urlpatterns = [
-    path('registration/', registration_form, name='registration_form'),
+    path('', registration_form, name='registration_form'),
     path('confirmation/<int:registration_id>/', confirmation_page, name='confirmation'),
 ]
